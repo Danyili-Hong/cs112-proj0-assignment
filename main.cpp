32d853e1ab2ce978d34974cabb27cf78d9d8bfcc
Danyili-Hong <dh48@calvin.edu>
2023-01-15 20:10:47 -0500

    Project0

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
-int main() {
+int main(){
+    float a[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    cout << "size of an array of 20 floats is" << sizeof(a) << endl;
 
-    return 0;
+    float *ptr = &a[7];
+    a[7] = 17.771;
+    cout << a[7] << endl;
+    cout << ptr << endl;
+
+
+
 }
